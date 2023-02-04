296b9c614814895bf3de09b09514bc5c41457324
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2023-02-04 19:07:27 +0900

    Save user database & passwords

--- hellotalk-nestjs/src/users/user.service.ts
+++ hellotalk-nestjs/src/users/user.service.ts
@@ -1,10 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpStatus, HttpException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { hashPassword } from 'src/utils/helpers';
+import { User } from 'src/utils/typeorm';
 import { CreateUserDetails } from 'src/utils/types';
+import { Repository } from 'typeorm';
 import { IUserService } from './user';
 
 @Injectable()
 export class UserService implements IUserService {
-     createUser(userDetails: CreateUserDetails) {
-          console.log('UserService.createUser');
+     constructor(
+          @InjectRepository(User) private readonly userRepository: Repository<User>,) { }
+
+     async createUser(userDetails: CreateUserDetails) {
+          const existingUser = await this.userRepository.findOneBy({
+               email: userDetails.email,
+          });
+          if (existingUser)
+               throw new HttpException('사용자가 이미 존재합니다.', HttpStatus.CONFLICT);
+          const password = await hashPassword(userDetails.password);
+          const newUser = this.userRepository.create({ ...userDetails, password });
+          return this.userRepository.save(newUser);
      }
 }
--- hellotalk-nestjs/src/users/users.module.ts
+++ hellotalk-nestjs/src/users/users.module.ts
@@ -2,13 +2,24 @@ import {
      Module
 }
 from '@nestjs/common';
-import {
-     Services
-}
-from 'src/utils/constants';
-import {
-     UserService
-}
-from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Services } from 'src/utils/constants';
+import { User } from 'src/utils/typeorm';
+import { UserService } from './user.service';
 
-@Module({ providers: [{ provide: Services.USERS, useClass: UserService, }, ], exports: [{ provide: Services.USERS, useClass: UserService, }, ], }) export class UsersModule {}
+@Module({
+     imports: [TypeOrmModule.forFeature([User])],
+     providers: [
+          {
+               provide: Services.USERS,
+               useClass: UserService,
+          },
+     ],
+     exports: [
+          {
+               provide: Services.USERS,
+               useClass: UserService,
+          },
+     ],
+})
+export class UsersModule { }
